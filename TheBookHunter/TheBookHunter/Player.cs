f90852d46b50bf4fa2dcbec7263bb2ad27024f62
shortcutsB <shortcutsb@gmail.com>
2021-07-26 19:13:31 +0900

    Player class 추가

---
+++ TheBookHunter/TheBookHunter/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheBookHunter
+{
+    class Player    //주인공 이름, 체력, 공격력
+    {
+        protected string name;
+        protected int hp = 100;
+        protected int attack = 10;
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ TheBookHunter/TheBookHunter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TheBookHunter
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Hunter    //주인공 이름, 체력, 공격력
+    {
+
+    }
+}

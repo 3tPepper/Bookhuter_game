c8e9a5e02930b49a3a35696eef810ac257473e51
shortcutsB <shortcutsb@gmail.com>
2021-07-27 18:16:53 +0900

    MixName method 추가

--- TheBookHunter/TheBookHunter/Player.cs
+++ TheBookHunter/TheBookHunter/Player.cs
@@ -14,5 +14,30 @@ namespace TheBookHunter
         {
             this.name = name;
         }
+
+        public string MixName(string name)  //이안의 잘못된 호명
+        {
+            string mix = name;
+            char[] mixTmp = mix.ToCharArray();//문자 배열 생성
+            for(int a=0; a < mix.Length; a++)
+            {
+                int b = a;
+                Random rand = new Random();
+                while (a == b)
+                {
+                    b = rand.Next(name.Length);
+                }
+                mix = new string(Swap(a, b, mixTmp));
+            }
+            return mix;
+        }
+
+        public char[] Swap(int a, int b, char[] mixTmp)
+        {
+            char tmp = mixTmp[a];
+            mixTmp[a] = mixTmp[b];
+            mixTmp[b] = tmp;
+            return mixTmp;
+        }
     }
 }

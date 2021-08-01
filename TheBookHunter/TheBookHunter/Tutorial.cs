8b55d81a0b93d8864bae64294abc868fcefa545d
3tPepper <3tpepperok@gmail.com>
2021-08-02 01:44:15 +0900

    tutorial update

--- TheBookHunter/TheBookHunter/Tutorial.cs
+++ TheBookHunter/TheBookHunter/Tutorial.cs
@@ -12,9 +12,15 @@ namespace TheBookHunter
         }
         public void GuideStart()
         {
-            Console.WriteLine("\n||---지하묘지 책 사냥꾼 가이드---||\n");
-
-            Console.WriteLine("[조작법: 해당하는 선택지의 숫자를 입력 후 Enter키를 누릅니다.]\n\n\n");
+            Console.WriteLine("\n[조작법: 해당하는 선택지의 숫자를 입력 후 Enter키를 누릅니다.]");
+            Console.WriteLine();
+            Console.WriteLine("┌──────────────────────────────┐");
+            Console.WriteLine("│                              │");
+            Console.WriteLine("│                              │");
+            Console.WriteLine("│   지하묘지 책 사냥꾼 가이드  │");
+            Console.WriteLine("│                              │");
+            Console.WriteLine("│                              │");
+            Console.WriteLine("└──────────────────────────────┘\n\n\n");
 
             bool stop = true;
             while(stop == true)
@@ -23,16 +29,17 @@ namespace TheBookHunter
                 string YOrN = Console.ReadLine();
                 if (YOrN == "1")
                 {
+                    stop = false;
                     //이름+설명
                 }
                 else if (YOrN == "2")
                 {
+                    stop = false;
                     //이름+스킵
                 }
                 else
                 {
-                    Console.WriteLine("이보쇼! 날 곤란하게 하고 싶은게 아니라면 1이나 2, 둘 중 하나만 골라서 입력하고 Enter 키를 누르도록 하라고!\n");
-                    stop = false;
+                    Console.WriteLine("\"이보쇼! 날 곤란하게 하고 싶은게 아니라면 1이나 2, 둘 중 하나만 선택하라고!\"\n\n");
                 }
             }
         }
